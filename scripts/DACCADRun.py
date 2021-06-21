39b512206dedfac6ab91113451b0399281620ae6
Mika Ito <g1720505@is.ocha.ac.jp>
2021-06-21 13:44:54 +0900

    modify

--- scripts/DACCADRun.py
+++ scripts/DACCADRun.py
@@ -41,10 +41,10 @@ def reservoir_eval_fn(daccadIndiv, config = {'daccad': {'path':'../../daccad'}},
     myarray = np.array(scaling)*np.array(daccadIndiv)
 
     # memory capacity
-    jikeiretu = submitDACCAD.submitPENSystem(myarray, nNodes = nNodes, executablePath=config['daccad']['path'],
+    jikeiretu = submitDACCAD.submitPENSystem_input(myarray, nNodes = nNodes, executablePath=config['daccad']['path'],
                                              configFile = os.path.abspath(os.getcwd())+"/"+config['daccad']['config_file'], 
                                              jsonFileName=os.path.abspath(os.getcwd())+"/"+config['dataDir']+"/"+config['daccad']['env_name']+datetime.now().isoformat(timespec='microseconds')+".json",
-                                             configFile_input = os.path.abspath(os.getcwd()) +"/"+config['config_file_input']
+                                             configFile_input = os.path.abspath(os.getcwd()) +"/"+config['daccad']['config_file_input']
                                              ).decode('ascii')
 
     Reservoir_ = Reservoir(ind=myarray, nNodes=nNodes, result=jikeiretu, inSize=1, outSize=1, delay=1, trainLen=1000, testLen=2000, initLen=200)
