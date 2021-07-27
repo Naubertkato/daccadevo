5c9523855ccc5be618b5db880e547699cbc7fa8c
Mika Ito <g1720505@is.ocha.ac.jp>
2021-07-27 14:03:18 +0900

    increase budget

--- scripts/DACCADRun.py
+++ scripts/DACCADRun.py
@@ -82,7 +82,7 @@ def reservoir_eval_fn(daccadIndiv, config = {'daccad': {'path':'../../daccad'}},
     X_gr, Y_gr = Reservoir_gr.run(data_for_gr)
     gene_rank = Reservoir_gr.get_KR_or_GR(X_gr, "gene")
 
-    print("[ {}: mc, {}: kr, {}: gr ]".format(mc, kernel_rank, gene_rank))
+    print("[ {}:size, {}: mc, {}: kr, {}: gr ]".format(nNodes, mc, kernel_rank, gene_rank))
     return [mc], [kernel_rank, gene_rank]
 
 class DACCADExperiment(QDExperiment):
