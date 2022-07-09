8371a6fcc513215ea4e8f0f58565f1488781e926
Mika Ito <g1720505@is.ocha.ac.jp>
2022-07-09 16:35:48 +0900

    fix

--- scripts/DACCADRun.py
+++ scripts/DACCADRun.py
@@ -153,6 +153,8 @@ class DACCADExperiment(QDExperiment):
                 self._eval_fn = reservoir_eval_fn
             elif self.config["eval"] == "reservoir_jacobian":
                 self._eval_fn = reservoir_jacobian_eval_fn
+            elif self.config["eval"] == "reservoir_jacobian_surrogate":
+                self._eval_fn = reservoir_jacobian_surrogate_eval_fn
             else:
                 factory = Factory()
                 self._eval_fn = factory[self.config["eval"]]
