25f414e1011b0a0647f1e6a8db74a5219a9deae9
Mika Ito <g1720505@is.ocha.ac.jp>
2022-07-11 14:52:30 +0900

    fix the shape of data

--- scripts/predict_mc.py
+++ scripts/predict_mc.py
@@ -8,6 +8,7 @@ import os
 
 def get_predict_mc(myarray):
     new_model = tf.keras.models.load_model(os.path.abspath(os.getcwd()) + '/scripts/notebook/saved_model/my_model')
+    myarray = myarray.reshape([1, 155])
     mc_prediction = new_model.predict(myarray)
 
     return mc_prediction
@@ -16,6 +17,7 @@ def get_predict_mc_eigenvalue(myarray, eigenvalue):
 
     new_model = tf.keras.models.load_model(os.path.abspath(os.getcwd()) + '/scripts/notebook/saved_model/my_model_eigenvalue')
     nw_eigenvalue_data = np.append(myarray, eigenvalue)
+    nw_eigenvalue_data = nw_eigenvalue_data.reshape([1, 156])
     mc_prediction = new_model.predict(nw_eigenvalue_data)
 
     return mc_prediction
