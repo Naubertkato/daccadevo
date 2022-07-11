2d43d634a862c9f8509565e18195b81fad209128
Mika Ito <g1720505@is.ocha.ac.jp>
2022-07-11 12:47:31 +0900

    fix the path of mymodel

--- scripts/predict_mc.py
+++ scripts/predict_mc.py
@@ -4,17 +4,17 @@ from tensorflow.keras import Model
 from tensorflow.keras import layers
 
 import numpy as np
+import os
 
 def get_predict_mc(myarray):
-
-    new_model = tf.keras.models.load_model('./notebook/saved_model/my_model')
+    new_model = tf.keras.models.load_model(os.path.abspath(os.getcwd()) + '/scripts/notebook/saved_model/my_model')
     mc_prediction = new_model.predict(myarray)
 
     return mc_prediction
 
 def get_predict_mc_eigenvalue(myarray, eigenvalue):
 
-    new_model = tf.keras.models.load_model('./notebook/saved_model/my_model_eigenvalue')
+    new_model = tf.keras.models.load_model(os.path.abspath(os.getcwd()) + '/scripts/notebook/saved_model/my_model_eigenvalue')
     nw_eigenvalue_data = np.append(myarray, eigenvalue)
     mc_prediction = new_model.predict(nw_eigenvalue_data)
 
