65eb43756edbfe29b502d0d3e97bd5139c027ea3
Mika Ito <g1720505@is.ocha.ac.jp>
2022-11-16 02:53:49 +0900

    update file

--- scripts/evolver.py
+++ scripts/evolver.py
@@ -275,16 +275,16 @@ class DaccadBioneatMut_5(Evolution):
 
     def __init__(self, container: Container, budget: int,
             ind_domain: DomainLike,
-            nb_nodes_domain: Sequence[int] = [1, 7],
+            nb_nodes_domain: Sequence[int] = [5, 5],
             min_init_budget: int = 1,
             min_ind_found_in_init: int = 1,
             init_pb: float = 0.0,
             sel_pb: float = 1.0,
             prob_parameter_mut: float = 0.5,
-            prob_template_add: float = 0.25,
-            prob_template_del: float = 0.25,
+            prob_template_add: float = 0.2,
+            prob_template_del: float = 0.2,
             prob_signal_species_add: float = 0,
-            prob_inhibition_species_add: float = 0,
+            prob_inhibition_species_add: float = 0.1,
             nbConnActivationsDomain: Sequence[int] = [1, 7],
             nbConnInhibitionsDomain: Sequence[int] = [0, 6],
             nbConnDomain: Sequence[int] = [1, 13],
@@ -439,7 +439,7 @@ class DaccadBioneatMut_2step(Evolution):
         self.mut_pb = 0.8
         self.init_drift = init_drift
         self.count = 0
-        with open("./results/reservoir_jacobian_surrogate/final_20220729022424.p", "rb") as f:
+        with open("./results/reservoir_jacobian_surrogate/final_20221114171451.p", "rb") as f:
             self.data = pickle.load(f)
         
 
