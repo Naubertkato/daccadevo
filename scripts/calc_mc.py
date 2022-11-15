6c979831a2a2cd8869c26caae4e4c68801803c82
Mika Ito <g1720505@is.ocha.ac.jp>
2022-11-15 16:32:15 +0900

    update file

--- scripts/calc_mc.py
+++ scripts/calc_mc.py
@@ -16,7 +16,7 @@ from ReservoirRun import Reservoir
 import submitDACCAD
 
 
-with open("../results/reservoir_jacobian_surrogate/final_20221115054543.p", "rb") as f:
+with open("../results/reservoir_jacobian_surrogate/final_20221115071831.p", "rb") as f:
         data = pickle.load(f)
 
 mc_lst = []
