8f45d0f27ffde94309b9c9d85a9cc522ca05dcab
Mika Ito <g1720505@is.ocha.ac.jp>
2022-11-26 13:21:21 +0900

    get true mc value

--- scripts/calc_mc.py
+++ scripts/calc_mc.py
@@ -15,48 +15,61 @@ from individual import DaccadIndividual
 from ReservoirRun import Reservoir
 import submitDACCAD
 
+def get_mc(file_name, num):
+    with open("../results/reservoir_jacobian_surrogate/final_" + str(file_name) + ".p", "rb") as f:
+            data = pickle.load(f)
 
-with open("../results/reservoir_jacobian_surrogate/final_20221115155201.p", "rb") as f:
-        data = pickle.load(f)
+    mc_lst = []
+    config = {'daccad': {'path':'../../daccad'}}
+    scales = [1000.0,200.0]
+    npeaks = 1
+    ave_mc = 0
+    len_ind = len(data["container"])
 
-mc_lst = []
-config = {'daccad': {'path':'../../daccad'}}
-scales = [1000.0,200.0]
-npeaks = 1
-ave_mc = 0
-len_ind = len(data["container"])
+    for i, daccadIndiv in enumerate(data["container"]):
+        print("***** " + str(i+1) + "/" + str(len_ind) + " *****")
+        print(daccadIndiv)
+        nNodes = daccadIndiv.nb_nodes
+        scaling = [scales[0]]*nNodes+[scales[1]]*(nNodes*nNodes*(nNodes+1))
+        myarray = np.array(scaling)*np.array(daccadIndiv)
+        nTemplates = len([a for a in myarray[nNodes: nNodes + nNodes * nNodes] if a != 0])
+        print("nTemplates : ", nTemplates)
+        mc_lst = np.zeros(10)
 
-for i, daccadIndiv in enumerate(data["container"]):
-    print("***** " + str(i+1) + "/" + str(len_ind) + " *****")
-    print(daccadIndiv)
-    nNodes = daccadIndiv.nb_nodes
-    scaling = [scales[0]]*nNodes+[scales[1]]*(nNodes*nNodes*(nNodes+1))
-    myarray = np.array(scaling)*np.array(daccadIndiv)
-    nTemplates = len([a for a in myarray[nNodes: nNodes + nNodes * nNodes] if a != 0])
-    print("nTemplates : ", nTemplates)
-    mc_lst = np.zeros(10)
+        for i in range(10):
+            # memory capacity
+            jikeiretu = submitDACCAD.submitPENSystem_input(myarray, nNodes = nNodes, executablePath=config['daccad']['path'],
+                                                            configFile = os.path.abspath(os.getcwd())+"/" + '../daccadConf.conf', 
+                                                            jsonFileName=os.path.abspath(os.getcwd())+"/"+"."+"/"+datetime.now().isoformat(timespec='microseconds')+".json",
+                                                            configFile_input = os.path.abspath(os.getcwd())+"/" + "../config_mc.json"
+                                                            ).decode('ascii')
 
-    for i in range(10):
-        # memory capacity
-        jikeiretu = submitDACCAD.submitPENSystem_input(myarray, nNodes = nNodes, executablePath=config['daccad']['path'],
-                                                         configFile = os.path.abspath(os.getcwd())+"/" + '../daccadConf.conf', 
-                                                         jsonFileName=os.path.abspath(os.getcwd())+"/"+"."+"/"+datetime.now().isoformat(timespec='microseconds')+".json",
-                                                         configFile_input = os.path.abspath(os.getcwd())+"/" + "../config_mc.json"
-                                                         ).decode('ascii')
+            mc = 0
+            k_max = 100 # the maximum delay length # 100
+            for k in range(1, k_max + 1):
+                Reservoir_mc = Reservoir(nNodes=nNodes, result=jikeiretu, delay=k)
+                data = Reservoir_mc.get_data()
+                X, Y = Reservoir_mc.run(data)
+                mc_k = Reservoir_mc.get_MCk(data, Y)
+                mc += mc_k
+            mc_lst[i] = mc
 
-        mc = 0
-        k_max = 100 # the maximum delay length # 100
-        for k in range(1, k_max + 1):
-            Reservoir_mc = Reservoir(nNodes=nNodes, result=jikeiretu, delay=k)
-            data = Reservoir_mc.get_data()
-            X, Y = Reservoir_mc.run(data)
-            mc_k = Reservoir_mc.get_MCk(data, Y)
-            mc += mc_k
-        mc_lst[i] = mc
+        ave_mc = np.mean(mc_lst)
+        std_mc = np.std(mc_lst)
 
-    ave_mc = np.mean(mc_lst)
-    std_mc = np.std(mc_lst)
+        print("ave_mc : ", ave_mc)
+        print("std_mc : ", std_mc)
+        print()
 
-    print("ave_mc : ", ave_mc)
-    print("std_mc : ", std_mc)
-    print()
+        mc_file_name = "mc_" + str(num) + "_" + str(file_name) + ".txt"
+        with open(mc_file_name, mode='w') as f:
+            f.write('\n'.join(mc_lst))
+
+if __name__ == "__main__":
+    file_name_lst = [20221123071403, 20221124082523, 20221124082754, 20221124083018, 20221124083336, 
+    20221124083524, 20221124083754, 20221124083940, 20221124084125, 20221124084353, 
+    20221124084917, 20221124121943, 20221124130546, 20221124131719, 20221124132007]
+    num = 31
+    for file_name in file_name_lst:
+        get_mc(file_name, num)
+        num += 1
