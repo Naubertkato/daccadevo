6c5bde0d97a391af95c6ee9b3a33c2b93029620d
Mika Ito <g1720505@is.ocha.ac.jp>
2023-01-21 22:04:03 +0900

    add new config file

--- scripts/DACCADRun.py
+++ scripts/DACCADRun.py
@@ -259,6 +259,8 @@ class DACCADExperiment(QDExperiment):
                 self._eval_fn = reservoir_surrogate_kernel_eval_fn
             elif self.config["eval"] == "reservoir_jacobian_2step":
                 self._eval_fn = reservoir_jacobian_eval_5_fn
+            elif self.config["eval"] == "reservoir_2step_kernel":
+                self._eval_fn = reservoir_jacobian_eval_5_fn
             else:
                 factory = Factory()
                 self._eval_fn = factory[self.config["eval"]]
